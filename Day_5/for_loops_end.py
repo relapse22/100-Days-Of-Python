058680e94fee82502098675513b097aa1a6d3e39
relapse22 <mich3al.stark3@gmail.com>
2020-12-09 17:00:37 +0000

    Adding Day_5 playing with loops and making password generator!

---
+++ Day_5/for_loops_end.py
@@ -0,0 +1,5 @@
+game_genres = ["Platformer", "RPG", "Racing", "Fighter"]
+for genre in game_genres:
+    print(genre)
+    print(genre + " game")
+print(game_genres)
---
+++ Day_5/galos.py
@@ -0,0 +1,5 @@
+# Adding all the numbers together from 1 to 100 
+total = 0
+for number in range(1, 101):
+    total += number
+print(total)
