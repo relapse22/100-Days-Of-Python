f301a542cef39e77ef3a420d8b288ee02e4dff3e
relapse22 <mich3al.stark3@gmail.com>
2020-12-09 16:36:15 +0000

    Day_4 playing with lists + Rock, paper, Scissors

---
+++ Day_4/lists.py
@@ -0,0 +1,5 @@
+Angelina = ["Queen", "Goddess", "LoveOfMyLife"]
+Mike = ["Dancer", "Coder", "Artist"] 
+abc = [Angelina, Mike]
+
+print(abc[1][1])
---
+++ Day_4/random_interger.py
@@ -0,0 +1,7 @@
+import random
+
+random_interger = random.randint(1, 10)
+print(random_interger)
+
+random_float = random.random() * 5
+print(random_float)
